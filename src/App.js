7a5c786b3b096eef5693fbbfb74fa0ff7f9d9641
kaiserawu <kaiser.a.wu@gmail.com>
2019-03-06 19:20:52 -0800

    Deleted unnecessary comments, day 1 mvp complete

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
     this.state = {
@@ -60,7 +57,6 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        {/* <input type='text' value={this.state.formText} onChange={this.handleFormText} /> */}
         <TodoForm formText={this.state.formText} handleFormText={this.handleFormText} handleSubmit={this.handleSubmit} />
         <TodoList listItems={this.state.todoData} />
       </div>
